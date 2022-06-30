0998f1b41cebf8dfe1603aa0220eb13a917da641
deepak08032002 <deepak.dk0803@gmail.com>
2022-06-30 23:10:02 +0530

    3rd Question

---
+++ Q3.java
@@ -0,0 +1,15 @@
+//Number of times sorted array is rotated
+
+public class Q3 {
+    public static void main(String[] args) {
+        int[] arr={11,12,15,18,2,3,6,7};
+        int n=arr.length;
+        int l=0;
+        int h=n-1;
+        while(h>l){
+            int mid=l+(h-l)/2;
+            if(arr[mid])
+        }
+    }
+
+}
