ace979035b6c098a6a378ff9307748263c3cad1f
deepak08032002 <deepak.dk0803@gmail.com>
2022-07-27 17:49:03 +0530

    Q19

---
+++ Q19.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class Q19 {
+    public static void main(String[] args) {
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        String s = "Leetcode";
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch) && map.get(ch) == 1) {
+                System.out.println(i);
+                break;
+            }
+        }
+        System.out.println(-1);
+    }
+
+}
